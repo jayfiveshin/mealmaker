e8abb7cce9a6ed232b3c48046959f62cabd29993
Joonha Shin <shin.joonha@gmail.com>
2012-03-04 21:31:18 -0500

    first commit

---
+++ mealmaker/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mealmaker::Application.config.secret_token = '8dd7348a4bdb2fc7218043fbdd7b2c16a4447fb2a5d75aff2fa292b43a365e9197050da9ea5f69994edf503f6d8b356696ca9977024bf20d0ca987474874cf44'
